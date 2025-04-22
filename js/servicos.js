a8d2e682174eed107b5941be2fac45ae21a19543
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-04-22 14:34:45 -0300

    042225 3

---
+++ js/servicos.js
@@ -0,0 +1,42 @@
+const servicosBarbearia = [
+    {
+      nome: "Corte Social",
+      descricao: "Corte de cabelo com tesoura ou máquina, finalização com produto.",
+      preco: 20.00
+    },
+    {
+        nome: "Corte Degradê",
+        descricao: "Corte de cabelo com tesoura ou máquina, finalização com produto.",
+        preco: 25.00
+      },
+    {
+      nome: "Barba Completa",
+      descricao: "Modelagem e alinhamento da barba com navalha e shave.",
+      preco: 25.00
+    },
+    {
+      nome: "Corte + Barba",
+      descricao: "Combo de corte masculino e barba completa com cuidados especiais.",
+      preco: 65.00
+    },
+    {
+      nome: "Sobrancelha",
+      descricao: "Limpeza e alinhamento da sobrancelha com tesoura e navalha.",
+      preco: 15.00
+    },
+    {
+      nome: "Corte Infantil",
+      descricao: "Corte para crianças até 12 anos, com cuidado e paciência.",
+      preco: 35.00
+    },
+    {
+      nome: "Hidratação Capilar",
+      descricao: "Tratamento de hidratação profunda para cabelos ressecados.",
+      preco: 25.00
+    },
+    {
+      nome: "Pigmentação de Barba",
+      descricao: "Aplicação de pigmento para cobrir falhas na barba.",
+      preco: 35.00
+    }
+  ];
